4272486e3b956368afa64d69c5680756e9a148f3
Jakub Marciniszyn <jakub.marciniszyn@tfs.com>
2022-02-08 15:41:37 +0000

    adds server side API fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Seo from '../components/seo'
@@ -12,28 +13,59 @@ import {
     mb2,
 } from '../css/layout.module.css'
 
-const IndexPage = () => (
-    <Layout>
-        {' '}
-        <Seo title="Home" />
-        <div className={sectionXl}>
-            <div className={container}>
-                <h1 className={fontWeightLight}>
-                    Hello there!
-                    <br />
-                    <span className={fontWeightNormal}>
-                        I'm Jakub Marciniszyn
-                    </span>
-                </h1>
-                <h5 className={fontWeightLight}>
-                    Amateur photographer with 7 years of experience
-                </h5>
+import { gallery } from '../css/index.module.css'
+
+export const query = graphql`
+    {
+        allImages {
+            edges {
+                node {
+                    thumbnailUrl
+                    title
+                    url
+                }
+            }
+        }
+    }
+`
+
+const IndexPage = ({ data }) => {
+    const images = data.allImages.edges
+
+    return (
+        <Layout>
+            {' '}
+            <Seo title="Home" />
+            <div className={sectionXl}>
+                <div className={container}>
+                    <h1 className={fontWeightLight}>
+                        Hello there!
+                        <br />
+                        <span className={fontWeightNormal}>
+                            I'm Jakub Marciniszyn
+                        </span>
+                    </h1>
+                    <h5 className={fontWeightLight}>
+                        Amateur photographer with 7 years of experience
+                    </h5>
+                </div>
             </div>
-        </div>
-        <div className={`${container} ${mt2} ${mb2}`}>
-            <p>Coming soon!!</p>
-        </div>
-    </Layout>
-)
+            <div className={`${container} ${mt2} ${mb2}`}>
+                <div className={gallery}>
+                    {images.map((image, i) => {
+                        const imageData = image.node
+                        return (
+                            <img
+                                key={i}
+                                src={imageData.thumbnailUrl}
+                                alt={imageData.title}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
+        </Layout>
+    )
+}
 
 export default IndexPage
