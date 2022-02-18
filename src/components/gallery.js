8e3adc8a4edebf72d3bc99b84d51cee8379ec4df
Jakub Marciniszyn <jakub.marciniszyn@tfs.com>
2022-02-18 13:31:30 +0000

    sets correct states on component load

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -23,47 +23,20 @@ const CONFIG = [
 ]
 
 const Gallery = ({ images }) => {
-    const [columns, setColumns] = React.useState(0)
-    const [gallery, setGallery] = React.useState([])
+    const [columns, setColumns] = React.useState(getColumns())
+    const [gallery, setGallery] = React.useState(getGallery(columns, images))
 
     //re-render if col no changes
     React.useEffect(() => {
-        if (columns === 0) return
-        setGallery(() => {
-            const newArray = Array.from({ length: columns }, () => [])
-
-            const newImages = [...images, ...images]
-
-            for (let i = 0; i < newImages.length; i++) {
-                const col = i % columns
-                newArray[col].push(newImages[i].node)
-            }
-            return newArray
-        })
+        setGallery(getGallery(columns, images))
     }, [columns, images])
 
     React.useEffect(() => {
-        function handleResize() {
-            let cols = 1 //start with default count: 1
-
-            //we loop through config backwards to match the highest resolution first
-            for (let i = CONFIG.length - 1; i >= 0; i--) {
-                const { breakpoint, columns } = CONFIG[i]
-                //check if breakpoint is matched
-                if (
-                    window.matchMedia(`screen and (min-width: ${breakpoint}px)`)
-                        .matches
-                ) {
-                    cols = columns
-                    break //match found. break
-                }
-            }
-            setColumns(cols)
-        }
-
         window.addEventListener('resize', handleResize)
-        //initial column check
-        handleResize()
+
+        function handleResize() {
+            setColumns(getColumns())
+        }
 
         const lazyLoadInstance = new LazyLoad()
         lazyLoadInstance.update()
@@ -73,6 +46,36 @@ const Gallery = ({ images }) => {
             window.removeEventListener('resize', handleResize)
         }
     }, [])
+
+    function getGallery(columns, images) {
+        const newArray = Array.from({ length: columns }, () => [])
+
+        const newImages = [...images, ...images]
+
+        for (let i = 0; i < newImages.length; i++) {
+            const col = i % columns
+            newArray[col].push(newImages[i].node)
+        }
+        return newArray
+    }
+
+    function getColumns() {
+        let cols = 1 //start with default count: 1
+
+        //we loop through config backwards to match the highest resolution first
+        for (let i = CONFIG.length - 1; i >= 0; i--) {
+            const { breakpoint, columns } = CONFIG[i]
+            //check if breakpoint is matched
+            if (
+                window.matchMedia(`screen and (min-width: ${breakpoint}px)`)
+                    .matches
+            ) {
+                cols = columns
+                break //match found. break
+            }
+        }
+        return cols
+    }
 
     return (
         <>
