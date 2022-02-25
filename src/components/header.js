cb8b62b9f2d45bf8601afb4511c620cd8dc76973
Jakub Marciniszyn <jakub.marciniszyn@tfs.com>
2022-02-25 15:12:54 +0000

    removes hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,128 +1,22 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 
 // CSS Imports
-import {
-    container,
-    listUnstyled,
-    fontWeightNormal,
-    uppercase,
-    letterSpacing2,
-} from '../css/layout.module.css'
+import { container } from '../css/layout.module.css'
 
-import {
-    header,
-    headerContainer,
-    menu,
-    menuList,
-    isActive,
-    toggle,
-    logo,
-} from '../css/header.module.css'
+import { header, headerContainer, logo } from '../css/header.module.css'
 
 // LOGO
 import Logo from './logo'
 
-import { Squeeze as Hamburger } from 'hamburger-react'
-
-// Check if window is defined (so if in the browser or in node.js).
-const isBrowser = typeof window !== 'undefined'
-
 const Header = () => {
-    if (isBrowser) {
-        function syncHeight() {
-            document.documentElement.style.setProperty(
-                '--window-inner-height',
-                `${window.innerHeight}px`
-            )
-        }
-
-        window.addEventListener('resize', syncHeight)
-        syncHeight()
-    }
-
-    const [isOpen, setOpen] = useState(false)
-    const modal = React.createRef()
-
-    useEffect(() => {
-        if (isOpen) {
-            //SHOW MENU
-            document.documentElement.classList.add('no-scroll')
-
-            // block pointer events
-            modal.current.addEventListener('pointermove', preventDefault)
-            modal.current.addEventListener('touchmove', preventDefault)
-        } else {
-            //HIDE MENU
-            document.documentElement.classList.remove('no-scroll')
-
-            // resume pointer events
-            modal.current.removeEventListener('pointermove', preventDefault)
-            modal.current.removeEventListener('touchmove', preventDefault)
-        }
-    }, [isOpen, modal])
-
-    // helper function to run preventDefault
-    function preventDefault(e) {
-        e.preventDefault()
-    }
-
     return (
-        <header className={`${header} ${isOpen ? isActive : ''}`}>
+        <header className={header}>
             <div className={`${container} ${headerContainer}`}>
                 <div className={logo}>
                     <Link to="/">
                         <Logo height="1rem" />
                     </Link>
-                </div>
-                <div className={toggle}>
-                    <Hamburger
-                        toggled={isOpen}
-                        toggle={setOpen}
-                        size={16}
-                        hideOutline={true}
-                        color={'var(--charcoal)'}
-                    />
-                </div>
-            </div>
-            <div className={`${menu}`} ref={modal}>
-                <ul className={`${listUnstyled} ${menuList}`}>
-                    <li>
-                        <h6
-                            className={`${fontWeightNormal} ${uppercase} ${letterSpacing2}`}
-                        >
-                            <a href="/">Home</a>
-                        </h6>
-                    </li>
-                    <li>
-                        <h6
-                            className={`${fontWeightNormal} ${uppercase} ${letterSpacing2}`}
-                        >
-                            <a href="/">About</a>
-                        </h6>
-                    </li>
-                    <li>
-                        <h6
-                            className={`${fontWeightNormal} ${uppercase} ${letterSpacing2}`}
-                        >
-                            <a href="/">Portfolio</a>
-                        </h6>
-                    </li>
-                    <li>
-                        <h6
-                            className={`${fontWeightNormal} ${uppercase} ${letterSpacing2}`}
-                        >
-                            <a href="/">Shop</a>
-                        </h6>
-                    </li>
-                </ul>
-                <div className="col-12 col-md-6">
-                    <h5 className={fontWeightNormal}>Contact Info:</h5>
-                    <ul className={listUnstyled}>
-                        <li>123 Example Rd, Example City PostCode</li>
-                        <li>contact@example.com</li>
-                    </ul>
                 </div>
             </div>
         </header>
